b00638f043593fb42a392950b6c2ef08286f7a2c
sssungjin <dkdlwls576@gmail.com>
2024-08-20 11:42:10 +0900

    chore: first commit

---
+++ src/test/java/com/kcs/security_sample/SecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcs.security_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
